8d199fe799baec4e3980c5cfa9024b52026d99f2
qqri <cqo1154@gmail.com>
2019-10-06 18:38:59 +0900

    spring

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,8 @@
-from rest_framework import serializers
-from api.models import Post, Comment
+
+#from rest_framework import serializers
+#from api.models import Post, Comment
 
 
-class PostSerializer(serializers.ModelSerializer):
+#class PostSerializer(serializers.ModelSerializer):
     # 여기를 구현해주시면 됩니다
+#*/
